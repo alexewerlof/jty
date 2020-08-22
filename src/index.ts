4410759f614324a6e1dfd3f69e3991c8c1aaa5db
Alex Ewerlöf <hanifbbz@gmail.com>
2020-08-22 11:02:19 +0200

    improve the documentation

--- src/index.ts
+++ src/index.ts
@@ -39,12 +39,49 @@ const {
 }
 
 /**
+ * Checks if the provided value is defined
+ * Also see [[isUndef]]
+ * 
+ * @param x any value
+ */
+export function isDef(x: unknown): boolean {
+  return x !== undefined;
+}
+
+/**
+ * Checks if the provided value is defined
+ * Also see [[isDef]]
+ * 
+ * @param x any value
+ */
+export function isUndef(x: unknown): x is undefined {
+  return x === undefined;
+}
+
+/**
+ * Checks if a value is a non-null object
+ * @param x possibly an object
+ */
+export function isObj(x: unknown): x is object {
+  return x !== null && typeof x === 'object'
+}
+
+/**
+ * Checks if a value is a function
+ * @param x possibly a function (including static methods but not getters/setters)
+ */
+export function isFn<T extends Function>(x: unknown): x is T {
+  return typeof x === 'function'
+}
+
+/**
  * Checks if a value is a finite number and optionally bound by a min and max
+ * 
  * @param x possibly a number
  * @param min the minimum possible value (inclusive). If this is not a finite number, the lower bound will not be checked
  * @param max the maximum possible value (inclusive). If this is not a finite number, the upper bound will not be checked
  */
-export function isBound(x: unknown, min?: number, max?: number): x is number {
+export function isNum(x: unknown, min?: number, max?: number): x is number {
   if (!isFinite(x)) {
     return false
   }
@@ -68,56 +105,65 @@ export function isBound(x: unknown, min?: number, max?: number): x is number {
 }
 
 /**
- * Checks if a value is a non-null object
- * @param x possibly an object
+ * Checks if a value is a finite integer number and optionally bound by a min and max.
+ * @example 3.14 is not an integer but 3 is
+ * 
+ * @param x possibly an integer number
+ * @param min the minimum possible value (inclusive). If this is not a finite number, the lower bound will not be checked
+ * @param max the maximum possible value (inclusive). If this is not a finite number, the upper bound will not be checked
  */
-export function isObj(x: unknown): x is object {
-  return x !== null && typeof x === 'object'
+export function isInt(x: unknown, min?: number, max?: number): x is number {
+  return isInteger(x) && isNum(x, min, max);
 }
 
 /**
- * Checks if a value is a function
- * @param x possibly a function (including static methods but not getters/setters)
+ * Checks if the provided value is boolean (basically `true` or `false`)
+ * 
+ * @param x possibly a boolean value
  */
-export function isFn<T extends Function>(x: unknown): x is T {
-  return typeof x === 'function'
-}
-
-export function isNum(x: unknown, min?: number, max?: number): x is number {
-  return isBound(x, min, max);
-}
-
-export function isInt(x: unknown, min?: number, max?: number): x is number {
-  return isInteger(x) && isBound(x, min, max);
-}
-
 export function isBool(x: unknown): x is boolean {
   return typeof x === "boolean";
 }
 
+/**
+ * Checks if the provided value is a string and optionally checks whether its length is in a boundary
+ * 
+ * @param x possibly a string
+ * @param minLen minimum possible length (inclusive)
+ * @param maxLen maximum possible length (inclusive)
+ */
 export function isStr(x: unknown, minLen = 0, maxLen?: number): x is string {
-  return typeof x === 'string' && isBound(x.length, minLen, maxLen)
-}
-
-export function isArr(x: unknown, minLen = 0, maxLen?: number): x is unknown[] {
-  return isArray(x) && isBound(x.length, minLen, maxLen)
+  return typeof x === 'string' && isNum(x.length, minLen, maxLen)
 }
 
 /**
- * Checks if x is a positive integer value that is a valid index to the specified array or string
+ * Checks if the provided value is an array and optionally checks whether its length is in a boundary
+ * 
+ * @param x possibly a string
+ * @param minLen minimum possible length (inclusive)
+ * @param maxLen maximum possible length (inclusive)
+ */
+export function isArr(x: unknown, minLen = 0, maxLen?: number): x is unknown[] {
+  return isArray(x) && isNum(x.length, minLen, maxLen)
+}
+
+/**
+ * Checks if x is a positive integer value that is a valid index to the specified array or string (based on their length).
+ * Also see [[hasProp]]
+ * 
+ * @param x possibly a positive integer
+ * @param target the target string or array
  */
 export function isIdx(x: unknown, target: string | Array<unknown>): x is number {
   return isInt(x, 0) && (isArr(target, x + 1) || isStr(target, x + 1))
 }
 
-export function isDef(x: unknown): boolean {
-  return x !== undefined;
-}
-
-export function isUndef(x: unknown): x is undefined {
-  return x === undefined;
-}
-
+/**
+ * Checks if x is a non-null object that has all the provided properties
+ * 
+ * @param x an object
+ * @param propNames one or more property names
+ */
 export function hasProp<K extends string | number | symbol>(
   x: unknown,
   ...propNames: K[]
@@ -135,6 +181,12 @@ export function hasProp<K extends string | number | symbol>(
   return true
 }
 
+/**
+ * Same as [[hasProp]] but checks for own properties (not inherited properties)
+ * 
+ * @param x an object
+ * @param propNames one or more property names
+ */
 export function hasOProp<K extends string | number | symbol>(
   x: unknown,
   ...propNames: K[]
@@ -152,6 +204,19 @@ export function hasOProp<K extends string | number | symbol>(
   return true
 }
 
+/**
+ * Checks if the provided value has the a path of properties
+ * 
+ * @example given `x = { foo: { bar: { baz: undefined }}}`
+ * 
+ * * `hasPath(x, 'foo', 'bar')` returns `true`
+ * * `hasPath(x, 'foo', 'bar', 'baz')` returns `true` because `x.foo.bar.baz` property exists
+ * * `hasPath(x, 'foo', 'hello', 'baz')` returns `false` because `x.foo.hello` property does not exist
+ * * `hasPath(x, 'foo', 'bar', 'hello')` returns `false`
+ * 
+ * @param x a value that may possibly have some properties
+ * @param propNames one or more property names
+ */
 export function hasPath(x: unknown, ...propNames: string[]) {
   let scope = x
 
@@ -170,6 +235,12 @@ export function hasPath(x: unknown, ...propNames: string[]) {
   return true
 }
 
+/**
+ * Similar to [[hasPath]] but only works for own properties (not inherited properties)
+ *
+ * @param x a value that may possibly have some properties
+ * @param propNames one or more property names
+ */
 export function hasOPath(x: unknown, ...propNames: string[]) {
   let scope = x
 
